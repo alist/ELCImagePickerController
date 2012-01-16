b1d498c12042e5ecfe15f2d289ae9dafdf5c3dd4
Alexander List <alist@listprojects.com>
2012-01-16 17:51:55 -0500

    fixed what's needed to stop multiple albums(hopefully)

--- Classes/cameraEnabledAlbumPickerController.m
+++ Classes/cameraEnabledAlbumPickerController.m
@@ -52,6 +52,11 @@
 	[super release];
 }
 
+-(void)viewWillAppear:(BOOL)animated{
+	[super viewWillAppear:animated];
+	[self.tableView reloadData];
+}
+
 -(void)	assetLibraryChanged: (id) sender{
 	[self.navigationItem setTitle:@"Loading..."];
 	[self.assetGroups removeAllObjects];
@@ -60,9 +65,6 @@
 	//	[self.navigationItem setRightBarButtonItem:cancelButton];
 	//	[cancelButton release];
 	
-    NSMutableArray *tempArray = [[NSMutableArray alloc] init];
-	self.assetGroups = tempArray;
-    [tempArray release];
     
     library = [[ALAssetsLibrary alloc] init];      
 	
